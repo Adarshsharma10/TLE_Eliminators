a8b8fb028a00174eb1461c526ecb1aab3c37369b
ADARSH SHARMA <sharmastudy27@gmail.com>
2026-03-03 12:31:58 +0530

    permutation

---
+++ Rated_800/20_Twin_permutation.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(v) (v).begin(), (v).end()
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for(auto &x : v){
+        cin>>x;
+    }
+    int m = n+1;
+    for(int i=0;i<n;i++){
+        int x = m-v[i];
+        cout<<x<<" ";
+    }cout<<endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
+// Successfully Submitted
