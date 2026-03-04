f60d1d1aa7b18f118c4b3c7841d18b290a197cbf
ADARSH SHARMA <sharmastudy27@gmail.com>
2026-03-04 21:24:14 +0530

    sol

--- Rated_800/28_Make_it_Beautiful.cpp
+++ Rated_800/28_Make_it_Beautiful.cpp
@@ -1,18 +1,41 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
+
+#define ll long long
+#define all(v) (v).begin(), (v).end()
+
+void solve() {
         int n;
         cin>>n;
         vector<int> v(n);
         for(int i=0;i<n;i++){
             cin>>v[i];
         }
-        for(int i=1;i<n-1;i++){
-          
+        set<int> s(v.begin(),v.end());
+        if(s.size()==1){
+            cout<<"NO"<<endl;
+            return;
         }
-
-    }
+        cout<<"YES"<<endl;
+        sort(v.begin(),v.end());
+        cout<<v[n-1]<<" ";
+        for(int i=0;i<n-1;i++){
+            cout<<v[i]<<" ";
+        }cout<<endl;
 }
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
+
+// Successfully Submitted
