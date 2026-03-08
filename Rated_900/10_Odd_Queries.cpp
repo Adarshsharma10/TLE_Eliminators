86f67f7be36a89530aad2205d77dca3bbc07e8ac
ADARSH SHARMA <sharmastudy27@gmail.com>
2026-03-08 23:51:20 +0530

    prefix

--- Rated_900/10_Odd_Queries.cpp
+++ Rated_900/10_Odd_Queries.cpp
@@ -12,10 +12,22 @@ int main(){
             cin>>v[i];
         }
         ll l,r,k;
+        vector<ll> prefix(n+1,0);
+        for(int i=0;i<n;i++){
+            prefix[i+1]=prefix[i]+v[i];
+        }
+        ll sum = accumulate(v.begin(),v.end(),0LL);
         while(q--){
             cin>>l>>r>>k;
-            
-
+            int diff = prefix[r]-prefix[l-1];
+            int m = r-l+1;
+            int ans = sum-diff+(m*k);
+            if(ans&1){
+                cout<<"YES"<<endl;
+            }
+            else{
+                cout<<"NO"<<endl;
+            }
 
 
 
