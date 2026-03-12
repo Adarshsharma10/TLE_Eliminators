3177d3e295238f1dcbebca34a87a3c26a96e0e64
ADARSH SHARMA <sharmastudy27@gmail.com>
2026-03-12 22:43:14 +0530

    Bitmasking***

---
+++ Rated_1000/17_Roof_Construction.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(v) (v).begin(), (v).end()
+
+void solve()
+{
+    int n;
+    cin >> n;
+    n--;
+    ll msb = log2(n);
+
+    vector<ll> ans;
+    ll num = pow(2, msb) - 1;
+    while (num >= 0)
+    {
+        ans.push_back(num);
+        num--;
+    }
+    num = pow(2, msb);
+    while (num <= n)
+    {
+        ans.push_back(num);
+        num++;
+    }
+
+    for (auto it : ans)
+        cout << it << " ";
+    cout << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+// Successfully Submitted
