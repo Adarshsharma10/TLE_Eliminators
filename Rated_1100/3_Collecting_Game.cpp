c5fa8260e38d9ae9f3757dbc4deedf7bab0f8964
ADARSH SHARMA <sharmastudy27@gmail.com>
2026-03-13 17:18:15 +0530

    greedy

--- Rated_1100/3_Collecting_Game.cpp
+++ Rated_1100/3_Collecting_Game.cpp
@@ -8,35 +8,36 @@ void solve() {
     int n;
     cin >> n;
     int oper = n;
-    vector<ll> v(n);
-    for(auto &x : v) cin>>x;
+    vector<ll> c(n);
+    for(auto &x : c) cin>>x;
+    vector<ll> v(c.begin(),c.end());
     sort(v.begin(),v.end());
     
-    vector<ll> pref(n);
-    pref[0]=v[0];
-    for(int i=1;i<n;i++){
-        pref[i]=pref[i-1]+v[i];
+    vector<ll> pref(n+1,0);
+    for(int i=1;i<=n;i++){
+        pref[i]=pref[i-1]+v[i-1];
     }
-    vector<int> ans;
-    ll score;
-    for(int i=0;i<n;i++){
-        ll cnt=0;
-        score+=v[i];
-        if(i>=1){
-            cnt=i;
+
+    vector<ll> ans(n);
+    ans[n-1]=n-1;
+
+    for(int i=n-2;i>=0;i--){
+        if(pref[i+1]>=v[i+1]){
+            ans[i]=ans[i+1];
         }
-        for(int j=i+1;j<n;j++){
-            if(score>=v[j]){
-                cnt++;
-                score
-            }
-            else{
-                ans.push_back(cnt);
-                break;
-            }
+        else{
+            ans[i]=i;
         }
     }
 
+    map<ll, int> p;
+    for(int i = 0; i < n; i++) {
+        p[v[i]] = ans[i];
+    }
+
+    for(int i = 0; i < n; i++) {
+        cout << p[c[i]] << (i == n - 1 ? "" : " ");
+    }cout<<endl;
 }
 
 int main() {
@@ -52,3 +53,4 @@ int main() {
     return 0;
 }
 
+// Successfully Submitted
