2189ea9ef45446acd43e9a3582defa3015a95ac6
ADARSH SHARMA <sharmastudy27@gmail.com>
2026-05-12 23:13:20 +0530

    900

---
+++ Rated_900/22. Luntik and Subsequences.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+int main()
+{
+	ll t;
+	cin >> t;
+	while (t--)
+	{
+		ll n;
+		cin >> n;
+		vector<ll> a(n);
+		for (ll i = 0; i < n; i++) 
+			cin >> a[i];
+
+		ll count_of_ones = 0; 
+		ll count_of_zeros = 0;
+		for (ll i = 0; i < n; i++) 
+		{
+			if (a[i] == 1)
+				count_of_ones++;
+			else if (a[i] == 0)
+				count_of_zeros++;
+		}
+
+		ll ways = pow(2, count_of_zeros) * count_of_ones;
+		cout << ways << endl;
+	}
+	return 0;
+}
+
+
+// Successfully Submitted
