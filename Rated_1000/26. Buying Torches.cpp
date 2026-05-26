be3a2824f679d7d86ebe0a24bb367dba8e5dc543
ADARSH SHARMA <sharmastudy27@gmail.com>
2026-05-26 23:52:26 +0530

    need changes

---
+++ Rated_1000/26. Buying Torches.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(v) (v).begin(), (v).end()
+
+void solve() {
+    ll x,y,k;
+    cin>>x>>y>>k;
+
+    ll total_sticks = y*k + k;
+    
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
